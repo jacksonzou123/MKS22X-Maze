408d6e77c61a1b6a075a119e1646fcfb84f8534e
Jackson Zou <jzou00@stuy.edu>
2019-02-26 11:02:51 -0500

    woorking on adding to array

--- Maze.java
+++ Maze.java
@@ -5,19 +5,37 @@ import java.util.Scanner;
 public class Maze{
   public static void main(String[] args) {
     try {
-      readFile("Maze.txt");
+      printFile("Maze.txt");
     }
     catch (FileNotFoundException e) {
       System.out.println("File not found");
     }
   }
 
-  public static void readFile(String filename) throws FileNotFoundException{
+  public static void printFile(String filename) throws FileNotFoundException{
     File text = new File("Maze.txt");
     Scanner reader = new Scanner(text);
     while (reader.hasNextLine()) {
       String line = reader.nextLine();
       System.out.println(line);
+    }
+  }
+
+  public static void intoArray(String filename) throws FileNotFoundException{
+    File text = new File("Maze.txt");
+    Scanner reader = new Scanner(text);
+    int lines = 0;
+    int count = 0;
+    while (reader.hasNextLine()) {
+      count = reader.nextLine();
+      lines++;
+    }
+    char[][] ary = new char[lines][count];
+    reader.reset();
+    int lines = 0;
+    while (reader.hasNextLine()) {
+      String line = reader.NextLine() 
+
     }
   }
 }
